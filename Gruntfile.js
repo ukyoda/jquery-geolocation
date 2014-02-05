34d2b71f216f5204774d747c9447985e480d2164
ukyoda <black12172001@hotmail.co.jp>
2014-02-05 21:21:19 +0900

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+		// Task configuration.
+		concat: {
+			options:{
+			},
+			basic: {
+				files:{
+					"dist/jquery.geolocation.js":[
+						"libs/intro.js",
+						"libs/geolocation.js",
+						"libs/outro.js"
+					]
+				}
+			}
+		},
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				unused: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true,
+					window: true
+				}
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib_test: {
+				src: ['lib/**/*.js', 'test/**/*.js']
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'concat' ]);
+
+};
---
+++ dist/jquery.geolocation.js
@@ -0,0 +1,41 @@
+(function($){
+
+var navigator = {};
+if(window.navigator){
+	navigator = window.navigator;
+} else {
+	navigator = {};
+}
+
+
+$.currentPosition = function(){
+	var deferred = $.Deferred();
+	if(!navigator.getCurrentPosition){
+		deferred.reject({
+			status:404,
+			message:"You can't use Geolocation API.";
+		});
+	} else {
+		navigator.getCurrentPosition(
+			function(geoInfo){
+				deferred.resolve(geoInfo,{
+					status:200,
+					message:"OK"
+				});
+			},
+			function(err){
+				deferred.reject({
+					status:err.code,
+					message:err.message
+				});
+			}
+		);
+	}
+
+	return deferred;
+
+};
+
+
+
+}(jQuery));
